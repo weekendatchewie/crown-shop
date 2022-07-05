69a307e51a48cd0391fd22bad938dcb70fcb6c12
Christopher P <christopher.poncelet51@gmail.com>
2022-07-05 13:43:14 +0200

    add logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import {Link, Outlet} from 'react-router-dom';
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 
 function Navigation() {
     return (
         <>
             <div className="navigation">
-                <h1>Home</h1>
+                <Link className="logo-container" to="/">
+                    <Logo />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="/shop">Shop</Link>
+                </div>
             </div>
-            <Outlet />
+            <Outlet/>
         </>
     );
 }
